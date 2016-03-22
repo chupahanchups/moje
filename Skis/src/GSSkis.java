348de9a674a5250795360be9ce693c5796fada6f
Hanna <h.nykowska@gmail.com>
2016-03-22 09:47:19 +0100

    2015-03-22 Hanna Nykowska adding last files

---
+++ Skis/src/GSSkis.java
@@ -0,0 +1,23 @@
+
+public class GSSkis extends Skis implements Stability{
+	
+	@Override
+	public void goFast(){
+		this.setTurnRadius(20);
+		System.out.println("GSSkis go fast, turn radius:" + this.turnRadius);
+	}
+	
+	@Override
+	public void turn(){
+		this.turnRadius -= 2;
+		System.out.println("GSSkis turn, turn radius:" + this.turnRadius);
+	}
+	
+	public static void main(String[] args){
+		GSSkis gs = new GSSkis();
+		gs.setTurnRadius(20);
+		gs.turn();
+		gs.goFast();
+	}
+
+}
---
+++ Skis/src/Stability.java
@@ -0,0 +1,4 @@
+
+public interface Stability {
+	public void goFast();
+}
